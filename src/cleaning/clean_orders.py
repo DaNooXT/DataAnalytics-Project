ce461ef5549ba0d8841bd45a05e693404a29ed8e
DaNooXT <raulkbauer2009@gmail.com>
2026-08-02 18:10:25 -0300

    Adds order cleanup functionality and integrates it into the main workflow.

---
+++ src/cleaning/clean_orders.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+
+def clean_orders (df):
+    df_orders = df.copy()
+    
+    df_orders['quantidade'] = pd.to_numeric (
+        df_orders['quantidade'],
+        errors="coerce"
+    )
+
+    df_orders['data_pedido'] = pd.to_datetime (
+        df_orders['data_pedido'],
+        errors="coerce",
+        dayfirst=False
+    )
+
+    df_orders['valor_total'] = pd.to_numeric (
+        df_orders['valor_total'],
+        errors='coerce'
+    ).astype(float)
+
+    df_orders['valor_total'] = (
+        df_orders['valor_total']
+        .fillna (
+            df_orders['valor_total'].median()
+        )
+        .round(2)
+    )
+
+    df_orders["status"] = (
+        df_orders["status"]
+        .fillna("CANCELADO")
+        .str.strip()
+        .str.upper()
+        .str.replace(
+            r"(.)\1+",
+            r"\1",
+            regex=True
+        )
+    )
+
+    df_orders['frete'] = pd.to_numeric (
+        df_orders['frete'],
+        errors='coerce'
+    ).astype(float)
+
+    df_orders['frete'] = (
+        df_orders['frete']
+        .fillna (
+            df_orders['frete'].mean()
+        )
+        .round(2)
+    )
+
+    df_orders['pedidos_sem_data'] = (
+        np.where (
+            (df_orders['data_pedido'].isna()), 
+            "PEDIDO SEM DATA", 
+            "PEDIDO COM DATA"
+        )
+    )
+
+    df_orders['desconto'] = df_orders['desconto'].fillna(0)
+
+    df_orders = df_orders.dropna(subset=['quantidade'])
+
+    return df_orders
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import loaders.load_data as ld  
-from cleaning.clean_clients import clean_clients  
+from cleaning.clean_clients import clean_clients 
+from cleaning.clean_orders import clean_orders
 
 def main ():
     #load all tables
@@ -8,8 +9,9 @@ def main ():
     df_payments = ld.load_payments()
     df_products = ld.load_products()
 
-    #clean al tables
+    #clean allk tables
     df_clients = clean_clients(df_clients)
+    df_orders = clean_orders(df_orders)
 
     #export clean DataFrames
     df_clients.to_csv("data/clean/clients.csv", index=False)
